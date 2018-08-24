535cda151b74aa2bc395c33f0b6d4225d606af2a
Aareon Sullivan <askully13@gmail.com>
2018-08-24 13:56:21 -0700

    Update block.py

--- block.py
+++ block.py
@@ -1,16 +1,21 @@
-
-from base import *
+from base import sha, 
 import transaction as T
+
+
 class BlockTX(object):
     def __init__(self, txs):
         """ List of transactions (TX)"""
         self.txs = txs
+
     def hash(self):
         sha(self.serialize())
+
     def serialize(self):
         return str([s.serialize() for s in self.txs])
+
     def spends(self):
         return [t.parent_tx for t in self.txs]
+
     @staticmethod
     def deserialize(s):
         v = ast.literal_eval(s)
@@ -19,6 +24,8 @@ class BlockTX(object):
             return b
         else:
             raise ValueError("Malformed tx block")
+
+
 class BlockHeader(object):
     def __init__(self, height, block_hash, prev, reward_address, nonce=None):
         self.nonce = nonce
@@ -26,9 +33,9 @@ class BlockHeader(object):
         self.block_hash = block_hash
         self.prev = prev
         self.reward_address = reward_address
+
     def serialize(self):
         return str((self.nonce, self.height, self.block_hash, self.prev, self.reward_address))
-    
 
     @staticmethod
     def deserialize(s):
@@ -37,6 +44,6 @@ class BlockHeader(object):
             return BlockHeader(v[1], v[2], v[3], v[4], v[0])
         else:
             raise ValueError("Bad BlockHeader")
+
     def hash(self):
         return sha(self.serialize())
-
